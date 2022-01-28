373c6031a3b90e05ce6ac046d6eab63f10696a67
WalidMoultamiss <walidmoultamis@gmail.com>
2022-01-28 13:07:07 +0100

    fix map error

--- navigation.js
+++ navigation.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import Add from "./screens/Add";
 import Map from "./screens/Map";
+import Balab from "./screens/Balab";
 
 const screenOptions = {
   headerShown: false,
@@ -14,9 +15,10 @@ const Stack = createStackNavigator();
 const SignedInStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
+      <Stack.Navigator screenOptions={screenOptions} initialRouteName="Balab">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Add" component={Add} />
+        <Stack.Screen name="Balab" component={Balab} />
         <Stack.Screen name="Map" component={Map} />
       </Stack.Navigator>
     </NavigationContainer>
